8df9d5228b3832ed4c32d75e608a573ea8524251
NoaBabliki <51087030+NoaBabliki@users.noreply.github.com>
2019-06-21 15:48:53 +0300

    ההפעלה מפה

--- four_in_a_row.py
+++ four_in_a_row.py
@@ -1,3 +1,22 @@
+import tkinter as tk
+
+from ai import *
+from game import *
+from screen import *
+from board import *
+
+
+class FourInRow:
+
+    def __init__(self, screen):
+        self.__screen = screen
+
+
+    def run(self):
+        self.__screen.main()
+
+
 
 if __name__ == '__main__':
-    pass
+    four_in_a_row = FourInRow(Screen(Game()))
+    four_in_a_row.run()
